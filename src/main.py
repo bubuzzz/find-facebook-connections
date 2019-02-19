e277b115e487ab1de440fea936c94331273b59d3
Sang Dinh <sang.dinh@wizeline.com>
2019-02-19 16:44:01 +0700

    - adding algorithm, facebook client class - auto reformat python file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,41 @@
-from facebook import crawl_friends
+from facebook import FacebookClient
+from algorithms import bfs
+
+
+class Node:
+    def __init__(self, username, name=None):
+        self.username = username
+        self.name = name
+
+
+class FBTree:
+    cookies = None
+    solution_required = 1  # NUMBER FRIENDS REQUIRED
+    client = FacebookClient(cookies)
+
+    def __init__(self):
+        self.root = self.client.get_myself_username()
+        self.goals = None
+
+    def get_root(self):
+        return self.root
+
+    def set_goals(self, username):
+        children = self.client.get_children(username)
+        self.goals = children.values()
+        print 'goals: ', self.goals
+
+    def is_goal(self, username):
+        return True if username in self.goals else False
+
+    def get_children(self, username):
+        children = self.client.get_children(username)
+        return children.values()
+
 
 if __name__ == '__main__':
-    print crawl_friends()
-
-
+    # crawl_friends()
+    tree = FBTree()
+    tree.set_goals("kenichi.ogw")
+    results = bfs(tree)
+    # print results
